c8cb6d52461dce540c1a080410b586d0207b8f31
Omprakash Gunasekaran <omprakash@Omprakashs-MacBook-Pro.local>
2019-10-17 16:55:50 +0530

    With shared pref to control expiry or expiring soon check by date

--- app/src/main/java/com/days/kitchenstock/DailyJobService.java
+++ app/src/main/java/com/days/kitchenstock/DailyJobService.java
@@ -10,13 +10,20 @@ import android.app.job.JobService;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import com.days.kitchenstock.data.StockContentHelper;
 
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+
 public class DailyJobService extends JobService {
-    public static final int JOB_ID = 4;
+    public static final int JOB_ID = 5;
     private static final long ONE_DAY_INTERVAL = 60 * 1000L; // 1 Day
+    private static final String LAST_CHECKED = "last_checked";
+    public static final String DATE_KEY = "date";
 
     public static void schedule(Context context) {
         JobScheduler jobScheduler = (JobScheduler)
@@ -39,8 +46,16 @@ public class DailyJobService extends JobService {
     @Override
     public boolean onStartJob(final JobParameters params) {
         Log.println(Log.INFO, "omprak", "onStartJob");
-        int expired = StockContentHelper.getExpiredSinceCount(getApplicationContext(), null);
-        int expiringSoon = StockContentHelper.getExpiringSoonSinceCount(getApplicationContext(), null);
+        SharedPreferences lastCheckedSharedPref = getSharedPreferences(LAST_CHECKED, MODE_PRIVATE);
+        String lastCheckedString = lastCheckedSharedPref.getString(DATE_KEY, null);
+        Date lastChecked;
+        try {
+            lastChecked = lastCheckedString == null ? null : StockContentHelper.DATE_FORMATTER.parse(lastCheckedString);
+        } catch (ParseException e) {
+            lastChecked = null;
+        }
+        int expired = StockContentHelper.getExpiredSinceCount(getApplicationContext(), lastChecked);
+        int expiringSoon = StockContentHelper.getExpiringSoonSinceCount(getApplicationContext(), lastChecked);
         if (expired != 0 || expiringSoon != 0) {
             Intent intent = new Intent().setComponent(new ComponentName("com.days.kitchenstock", "com.days.kitchenstock.HomeActivity"));
             PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
@@ -54,6 +69,10 @@ public class DailyJobService extends JobService {
             notificationManager.notify(1, notification);
             Log.println(Log.INFO, "omprak", "Notified");
         }
+        SharedPreferences.Editor editor = lastCheckedSharedPref.edit();
+        editor.putString(DATE_KEY, StockContentHelper.DATE_FORMATTER.format(Calendar.getInstance().getTime()));
+        editor.apply();
+        editor.commit();
         return false;
     }
 
